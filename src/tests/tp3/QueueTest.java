bd2a56a1a9fcb293140653651edc99325f142dde
Lucas BONNAL <lbonnal@gmail.com>
2018-10-15 10:23:50 -0400

    Wont start

--- src/tests/tp3/QueueTest.java
+++ src/tests/tp3/QueueTest.java
@@ -2,13 +2,22 @@ package tests.tp3;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 class QueueTest {
 
     Queue<Integer> queue;
 
     @Test
-    void teneurdeplace() {
-        // TODO
+    void TR1SEQ1() {
+        queue = new Queue<Integer>();
+        assertNull(queue.peek());
+        queue.enqueue(1);
+        assertEquals(1, (int) queue.peek());
+        queue.dequeue();
+        assertTrue(queue.isEmpty());
+        queue.toString();
+        queue.iterator();
     }
 
 }
