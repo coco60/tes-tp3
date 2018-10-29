326a1b0113e90b0fb023214ac273ce9ce4a95970
Lucas BONNAL <lbonnal@gmail.com>
2018-10-29 09:17:17 -0400

    TP4, c'est un peu l'ennui

---
+++ src/TP4/QueueTestEtats.java
@@ -0,0 +1,19 @@
+package TP4;
+
+import org.junit.Test;
+import tests.tp3.Queue;
+
+import static org.junit.Assert.*;
+
+public class QueueTestEtats {
+
+    private Queue queue;
+
+    @Test
+    public void branche1() {
+
+
+
+    }
+
+}
